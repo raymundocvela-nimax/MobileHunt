31d197772a067a01b9cc0845d3db242834f9b320
Raymundo Castillo <raymundoc.vela@gmail.com>
2012-07-25 19:53:02 -0500

    Redirecci´onar Pag Restricción
    Ya redirecciona a la pagina tenga o no tenga restricción

--- getpuntosgl_1.php
+++ getpuntosgl_1.php
@@ -5,28 +5,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html style="height:100%" xmlns="http://www.w3.org/1999/xhtml"> <!--style="height:100%" es para poder ocupar el porcentaje en el div del mapa, si no se pone el div q se genera es de altura � height 0-->
-<head>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
-    <title> Hunt GPS - Proyecto Terminal Ingeniería en Computación UAM Azcapotzalco</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+        <title> Hunt GPS - Proyecto Terminal Ingeniería en Computación UAM Azcapotzalco</title>
 
-    <!-- API Google MAPS muestra configuración Mapa-->
-    <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
-    
-    <!-- Hoja de Estilo-->
-    <link href="Style/HuntGPS.css" rel="stylesheet" type="text/css" />
+        <!-- API Google MAPS muestra configuración Mapa-->
+        <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
 
-    <!--Favicon-->
-    <link REL="SHORTCUT ICON" HREF="image/Favicon.ico">
+        <!-- Hoja de Estilo-->
+        <link href="Style/HuntGPS.css" rel="stylesheet" type="text/css" />
 
-    <!--Reloj-->
+        <!--Favicon-->
+        <link REL="SHORTCUT ICON" HREF="image/Favicon.ico">
+
+        <!--Reloj-->
         <script type="text/javascript"><!--
-         var rutaKml=0;
-         var rutaLayer;
-         var it;//poligono
-         //var map;
-         var hayRestriccion;
-         var existLoc;
-        //se ponen <!-- por si el explorador no es compatibel con Javascript no salga impreso el codigo            
+            var rutaKml=0;
+            var rutaLayer;
+            var it;//poligono
+            //var map;
+            var hayRestriccion;
+            var existLoc;
+            //se ponen <!-- por si el explorador no es compatibel con Javascript no salga impreso el codigo            
             function HoraActual(hora, minuto, segundo){
                 segundo = segundo + 1;
                 if(segundo == 60) {
@@ -48,9 +48,9 @@
                 setTimeout("HoraActual("+hora+", "+minuto+", "+segundo+")", 1000);
             } 
 
-//    <!-- API Google MAPS muestra configuración Mapa-->
-            
- //     <!-- Primer Mapa usado-->
+            //    <!-- API Google MAPS muestra configuración Mapa-->
+
+            //     <!-- Primer Mapa usado-->
             function initialize() {
                 var latlng = new google.maps.LatLng(23.919722222222223, -102.1625); //Centro del Mapa
                 var settings = {
@@ -64,7 +64,7 @@
                 };
                 map = new google.maps.Map(document.getElementById("map_canvas"), settings);
             }
-           
+
             function loadkml(){
                 /* 
                 var latlng = new google.maps.LatLng(23.919722222222223, -102.1625); //Centro del Mapa
@@ -87,161 +87,163 @@
                 else alert("No hay Datos");
 
             }
-          
+
             function abrirPag(url){
-                if(hayRestriccion==1)
+                if(hayRestriccion==1){
                     mensaje=confirm("Ya existe una restricción, ¿deseas eliminarla y agregar una nueva?");
+                    if(mensaje) window.location.href = url; //abre la pagina en la misma ventana si da click en ACEPTAR
+                }
                 else window.location.href = url; //abre la pagina en la misma ventana
                 //window.open(url,"","algun parametro que desees"); abre la pagina en nueva ventana
             }
-            
+
             function showRestriccion(){
                 /*
                 var latlng = new google.maps.LatLng(23.919722222222223, -102.1625); //Centro del Mapa
                 var settings = {
-                    zoom: 6,
-                    center: latlng,
-                    mapTypeControl: true,
-                    mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-                    navigationControl: true,
-                    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                zoom: 6,
+                center: latlng,
+                mapTypeControl: true,
+                mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+                navigationControl: true,
+                navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+                mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
                 var map = new google.maps.Map(document.getElementById("map_canvas"), settings)
                 */
                 it = new google.maps.Polygon(polyOptions);
                 it.setMap(map);
             }
-            
+
             function funciones(){//Juntamos las funciones en una sola, para poderlas ejecutar en el onload del body
                 initialize();
                 HoraActual(<?php echo date("H",time()/*-3600*/).", ".date("i").", ".date("s"); ?>); //time()-3600 resta 1 hora al tiempo del servidor para ajustarlo a nuestra hora
             }
-          </script>
-</head>     
-<body onload="funciones()">
-<!-- dothird(); para cargar otra accion en el body -->
-	<div class="contenedor">
-    	<div class="encabezado">
-            <h1>Mobile Hunt - Proyecto Terminal
-             <br />Ingeniería en Computación UAM Azcapotzalco</h1>
-        </div>        
-        <div class="menu">
-			<div class="encabezadoMenu"><h1>Panel de Administración</h1>
-                <a href="javascript:history.go(-1)">&lt&ltatrás</a>
-                 | 
-                <a href="index.php">Principal</a>
-            	<div class="fechaMenu"><?php $fecha=date("d/m/y"/*." "."h:i:s"*/); echo $fecha;?> </div><!--fecha-->
-                <div class="relojMenu" id="relojMenu"></div>
-            </div>
-
-            <div class="formulario" id="formulario" style="overflow:auto">
- <!--           div form
-                <div class="usr" id="usr" style="overflow:auto">
-                    div usr
+        </script>
+    </head>     
+    <body onload="funciones()">
+        <!-- dothird(); para cargar otra accion en el body -->
+        <div class="contenedor">
+            <div class="encabezado">
+                <h1>Mobile Hunt - Proyecto Terminal
+                    <br />Ingeniería en Computación UAM Azcapotzalco</h1>
+            </div>        
+            <div class="menu">
+                <div class="encabezadoMenu"><h1>Panel de Administración</h1>
+                    <a href="javascript:history.go(-1)">&lt&ltatrás</a>
+                    | 
+                    <a href="index.php">Principal</a>
+                    <div class="fechaMenu"><?php $fecha=date("d/m/y"/*." "."h:i:s"*/); echo $fecha;?> </div><!--fecha-->
+                    <div class="relojMenu" id="relojMenu"></div>
                 </div>
--->                
-                <!-- verificamos si existe restricción guardada
-                -->                
-                <?php
-                    include('conectar.php');
-                    $idUsr=$_REQUEST['usr'];
-                    $_SESSION['idUsr']=$idUsr;
-                    $query="SELECT restriccion FROM usuarios WHERE idusuarios ='".$_SESSION['idUsr']."'";
-                    echo "<br>".$query."<br>";
-                    $result=mysql_query($query);
-                    $row=mysql_fetch_array($result);
-                    if($row[0]!=null){
-                        $js=$row[0];
-                        echo '<script type="text/javascript">'.$js.'</script>';                    
-                        echo '<button type="button" onclick="showRestriccion()" >Mostrar Restricción</button>';
-                        echo '<script type="text/javascript">hayRestriccion=1;</script>';                    
-                    }
-                    else{
-                      echo "<br>El usuario:".$_SESSION['usr']." no tiene restricción de área";
-                      echo '<script type="text/javascript">hayRestriccion=0;</script>';                    
-                    } 
-                 ?>
-                <button type="button" align="center" onclick="abrirPag('v3tool_restricciones.html')">Establecer Restricción</button> 
-                <button type="button" align="center" onclick="loadkml()">Ver ruta </button>
-                <?php obtenerDatos();?>
-            </div>     
+
+                <div class="formulario" id="formulario" style="overflow:auto">
+                    <!--           div form
+                    <div class="usr" id="usr" style="overflow:auto">
+                    div usr
+                    </div>
+                    -->                
+                    <!-- verificamos si existe restricción guardada
+                    -->                
+                    <?php
+                        include('conectar.php');
+                        $idUsr=$_REQUEST['usr'];
+                        $_SESSION['idUsr']=$idUsr;
+                        $query="SELECT restriccion FROM usuarios WHERE idusuarios ='".$_SESSION['idUsr']."'";
+                        echo "<br>".$query."<br>";
+                        $result=mysql_query($query);
+                        $row=mysql_fetch_array($result);
+                        if($row[0]!=null){
+                            $js=$row[0];
+                            echo '<script type="text/javascript">'.$js.'</script>';                    
+                            echo '<button type="button" onclick="showRestriccion()" >Mostrar Restricción</button>';
+                            echo '<script type="text/javascript">hayRestriccion=1;</script>';                    
+                        }
+                        else{
+                            echo "<br>El usuario:".$_SESSION['usr']." no tiene restricción de área";
+                            echo '<script type="text/javascript">hayRestriccion=0;</script>';                    
+                        } 
+                    ?>
+                    <button type="button" align="center" onclick="abrirPag('v3tool_restricciones.html')">Establecer Restricción</button> 
+                    <button type="button" align="center" onclick="loadkml()">Ver ruta </button>
+                    <?php obtenerDatos();?>
+                </div>     
+            </div>
+            <div class="mapa" id="map_canvas">Mapa</div>
+            <div class="pie">Pie</div>
         </div>
-        <div class="mapa" id="map_canvas">Mapa</div>
-        <div class="pie">Pie</div>
-	</div>
-	<?php
-    function obtenerDatos(){
-        include('conectar.php');
-        include('makerutakml_1.php');
-                    
-        //adecuar ID usr
-        $nomInsti=$_SESSION['nomInsti'];
-        //$_SESSION['idUsr']=$idUsr;
-        
-        //recuperar fecha
-        $fechaExplode=explode('-',$_REQUEST['fecha']);
-        //$fechaQuery=date('Y-m-d',mktime(0,0,0,$fechaExplode[1],$fechaExplode[0],$fechaExplode[2]));
-        $fechaQuery=date('d-m-Y',mktime(0,0,0,$fechaExplode[1],$fechaExplode[0],$fechaExplode[2]));
-        //echo $fechaQuery."<br>";
-        //print_r $fechaQuery;
-        
-        //obtener nombre Usr segun id
-        $result=mysql_query("SELECT usuario FROM usuarios WHERE idusuarios='".$_SESSION['idUsr']."'") or die("error".mysql_error());
-        $row=mysql_fetch_array($result);
-        $nomUsr=$row[0];
-        $_SESSION['nomUsr']=$nomUsr;
-        
-        echo"<br>Institucion/Compania:".$_SESSION['idInsti']."- $nomInsti <br>";
-        echo"Nombre de Usuario: ".$_SESSION['idUsr']." - $nomUsr <br>";
-        echo"Fecha:".$fechaQuery."<br>";
-        //puntos segun idUsr
-        /*consulta original
-        $result=mysql_query("SELECT idpuntos,longitud,latitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$idUsr."' AND fecha='".$fechaQuery."'") or die("error".mysql_error());
-        */
-        $query=mysql_query("SELECT idpuntos,latitud,longitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$_SESSION['idUsr']."' AND DATE_FORMAT(fecha,'%d-%m-%Y')='".$fechaQuery."'") or die("error".mysql_error());
-        //echo "consulta SELECT idpuntos,longitud,latitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$idUsr."' AND DATE_FORMAT(fecha,'%d-%m-%Y')='".$fechaQuery."')";        
-        $numRow=mysql_num_rows($query);
-        if($numRow!=0){            
-            //creamos tabla
-            echo "<table border = '1'> ";
-            //nombre filas
-            echo "<tr> ";
-            echo "<td><b>ID</b></td> ";
-            echo "<td><b>Latitud (y)</b></td> ";
-            echo "<td><b>Longitud (x)</b></td> ";
-            echo "<td><b>Fecha y hora</b></td> ";
-            echo "<td><b>Provedor de localización</b></td> ";
-            echo "</tr> ";
-            //datos      
-            while ($row=mysql_fetch_array($query)){
-                echo "<tr> ";
-                echo "<td>$row[0]</td> ";
-                echo "<td>$row[1]</td> ";
-                echo "<td>$row[2]</td> ";
-                echo "<td>$row[3]</td> ";
-                echo "<td>$row[4]</td> ";
-                echo "</tr> ";
-            }
-            echo "</table> ";
-            //creamos KML y obtenemos nombre archivo
-            $rutaKml=mk($_SESSION['idUsr'],$nomUsr,$nomInsti,$fechaQuery);
-            if($rutaKml!=null)
-            {
-                echo"<br>nombre kml: ".$rutaKml;
-                //pasamos valor a variable de javascript para mostrar kml
-                echo '<script type="text/javascript">rutaKml="'.$rutaKml.'"</script>';    
-                echo '<script type="text/javascript">existLoc=1;</script>';  
-            }
-            else 'Error en KML<script type="text/javascript">existLoc=0;</script>';
-            
-            
-        }
-        else{
-            echo '<script type="text/javascript">existLoc=0;</script>';       
-            echo '<b><br>No existen localizaciones asociadas a esta fecha:</b>'.$fechaQuery;
-        }        
-    }            
-?>            
-</body>
+        <?php
+            function obtenerDatos(){
+                include('conectar.php');
+                include('makerutakml_1.php');
+
+                //adecuar ID usr
+                $nomInsti=$_SESSION['nomInsti'];
+                //$_SESSION['idUsr']=$idUsr;
+
+                //recuperar fecha
+                $fechaExplode=explode('-',$_REQUEST['fecha']);
+                //$fechaQuery=date('Y-m-d',mktime(0,0,0,$fechaExplode[1],$fechaExplode[0],$fechaExplode[2]));
+                $fechaQuery=date('d-m-Y',mktime(0,0,0,$fechaExplode[1],$fechaExplode[0],$fechaExplode[2]));
+                //echo $fechaQuery."<br>";
+                //print_r $fechaQuery;
+
+                //obtener nombre Usr segun id
+                $result=mysql_query("SELECT usuario FROM usuarios WHERE idusuarios='".$_SESSION['idUsr']."'") or die("error".mysql_error());
+                $row=mysql_fetch_array($result);
+                $nomUsr=$row[0];
+                $_SESSION['nomUsr']=$nomUsr;
+
+                echo"<br>Institucion/Compania:".$_SESSION['idInsti']."- $nomInsti <br>";
+                echo"Nombre de Usuario: ".$_SESSION['idUsr']." - $nomUsr <br>";
+                echo"Fecha:".$fechaQuery."<br>";
+                //puntos segun idUsr
+                /*consulta original
+                $result=mysql_query("SELECT idpuntos,longitud,latitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$idUsr."' AND fecha='".$fechaQuery."'") or die("error".mysql_error());
+                */
+                $query=mysql_query("SELECT idpuntos,latitud,longitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$_SESSION['idUsr']."' AND DATE_FORMAT(fecha,'%d-%m-%Y')='".$fechaQuery."'") or die("error".mysql_error());
+                //echo "consulta SELECT idpuntos,longitud,latitud,fecha,provider FROM puntos WHERE usuarios_idUsuarios='".$idUsr."' AND DATE_FORMAT(fecha,'%d-%m-%Y')='".$fechaQuery."')";        
+                $numRow=mysql_num_rows($query);
+                if($numRow!=0){            
+                    //creamos tabla
+                    echo "<table border = '1'> ";
+                    //nombre filas
+                    echo "<tr> ";
+                    echo "<td><b>ID</b></td> ";
+                    echo "<td><b>Latitud (y)</b></td> ";
+                    echo "<td><b>Longitud (x)</b></td> ";
+                    echo "<td><b>Fecha y hora</b></td> ";
+                    echo "<td><b>Provedor de localización</b></td> ";
+                    echo "</tr> ";
+                    //datos      
+                    while ($row=mysql_fetch_array($query)){
+                        echo "<tr> ";
+                        echo "<td>$row[0]</td> ";
+                        echo "<td>$row[1]</td> ";
+                        echo "<td>$row[2]</td> ";
+                        echo "<td>$row[3]</td> ";
+                        echo "<td>$row[4]</td> ";
+                        echo "</tr> ";
+                    }
+                    echo "</table> ";
+                    //creamos KML y obtenemos nombre archivo
+                    $rutaKml=mk($_SESSION['idUsr'],$nomUsr,$nomInsti,$fechaQuery);
+                    if($rutaKml!=null)
+                    {
+                        echo"<br>nombre kml: ".$rutaKml;
+                        //pasamos valor a variable de javascript para mostrar kml
+                        echo '<script type="text/javascript">rutaKml="'.$rutaKml.'"</script>';    
+                        echo '<script type="text/javascript">existLoc=1;</script>';  
+                    }
+                    else 'Error en KML<script type="text/javascript">existLoc=0;</script>';
+
+
+                }
+                else{
+                    echo '<script type="text/javascript">existLoc=0;</script>';       
+                    echo '<b><br>No existen localizaciones asociadas a esta fecha:</b>'.$fechaQuery;
+                }        
+            }            
+        ?>            
+    </body>
 </html>
