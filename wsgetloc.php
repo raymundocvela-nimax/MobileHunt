41058282d0744f9f5263ccd768dad8f106f6e61f
Raymundo Castillo <raymundoc.vela@gmail.com>
2012-08-11 10:41:47 -0500

    Debug
    Quitando logs demás para debuggear mejor

--- wsgetloc.php
+++ wsgetloc.php
@@ -1,31 +1,34 @@
 <?php
     /*
-    Web service que recibe consulta la restricción de un determinado usuario, inserta una localización a la DB 
+    Web service que recibe consulta la restricción de un determinado usuario, inserta una localización a la DB
     y responde indicando _1| si se inserto o _0| en caso contrario + el código en JAvaScript de la restricción de área del usuario
     */
     include('conectar.php');
     $usr=$_REQUEST['usr'];
-    echo $usr;
+//    echo $usr;
     //obtener id usr
     $qUsr="SELECT idusuarios FROM usuarios WHERE usuario='$usr'";
-    echo "<br>".$qUsr;
+ //   echo "<br>".$qUsr;
     $result=mysql_query($qUsr);
     $idUsr=mysql_fetch_array($result);
     //obtenemos restricción
     $query="SELECT restriccion FROM usuarios WHERE idusuarios ='".$idUsr[0]."'";
-    echo "<br>".$query;
+//    echo "<br>".$query;
     $result=mysql_query($query);
     $row=mysql_fetch_array($result);
     if($row[0]!=NULL){
         //Si hay restriccion de área?
         $js=$row[0];
-        echo "<br>restricción: ".$js."--";
+        echo "<br>restricción: ".$js."--\n";
         $laty=$_REQUEST['laty'];//longitud
         $lonx=$_REQUEST['lonx'];//latitud
         //Crea el poligono
+        /*
         echo "<br>usr:".$_REQUEST['usr'];
         echo "<br>laty:".$_REQUEST['laty'];
         echo "<br>lonx:".$_REQUEST['lonx']."<br>";
+        */
+
         //Se pasa el valor de javascript
         //$inOut="<script>document.write(inOut)</script>";
     }
@@ -44,7 +47,7 @@
         $responsePhp="_0|".$js;
     }
     else $responsePhp="_1|".$js;
-    echo $responsePhp."phpdate: ".$phpdate."consulta: ".$query;
+    //echo $responsePhp."phpdate: ".$phpdate."consulta: ".$query;
     print(json_encode($responsePhp));
     mysql_close();
 ?>
