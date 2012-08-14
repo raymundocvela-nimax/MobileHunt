10c497e8a6ed8acccea016b389e2b88137966fd7
Raymundo Castillo <raymundoc.vela@gmail.com>
2012-08-14 00:56:44 -0500

    menos \r\n
    con preg_replace, se quitan los caracteres que no sirven para Javascript
    ejecutado por android

--- wsgetloc.php
+++ wsgetloc.php
@@ -19,6 +19,8 @@
     if($row[0]!=NULL){
         //Si hay restriccion de área?
         $js=$row[0];
+        $pattern=array("(\r\n)", "(\n\r)", "(\n)", "(\r)");
+        $js=preg_replace($pattern," ",$js);
 //        echo "<br>restricción: ".$js."--\n";
         $laty=$_REQUEST['laty'];//longitud
         $lonx=$_REQUEST['lonx'];//latitud
