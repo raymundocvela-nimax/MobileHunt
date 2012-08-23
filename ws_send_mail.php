2bc099d14d4f052884455c7640ea17f47788ad55
Raymundo Castillo <raymundoc.vela@gmail.com>
2012-08-22 23:46:36 -0500

    MailPHP
    \n por <br>
    Se quitan las consultas SQL, ya que podemos mandar la informacion vías
    WS
    Agregamos lineas para regresar responsePhp

--- ws_send_mail.php
+++ ws_send_mail.php
@@ -3,40 +3,30 @@
     $usr=$_REQUEST['usr'];
     $laty=$_REQUEST['laty'];
     $lonx=$_REQUEST['lonx'];
+    $nomInsti=$_REQUEST['comp'];
+    $desc=$_REQUEST['desc'];
+    $mail=$_REQUEST['mail'];
     $bestprov=$_REQUEST['bestprov'];
     $phpdate=date("Y-m-d H:i:s");
-    $qUsr="SELECT institucion_idinstitucion, descripcion, mail FROM usuarios WHERE usuario='$usr'";
-    $result=mysql_query($qUsr);
-    $row=mysql_fetch_array($result);
-    if($row[0]!=NULL) $idInsti=$row[0];
-    else $idInsti="Sin Dato";
-    if($row[1]!=NULL)$desc=$row[1];
-    else $desc="Sin Dato";
-    if($row[2]!=NULL) $mail=$row[2];
-    else $mail="raymundoc.vela@hotmail.com";
 
-    //obtenemos nom Insti
-    $query="SELECT nombre FROM institucion WHERE institucion_idinstitucion ='".$idInsti."'";
-    $result=mysql_query($query);
-    $row=mysql_fetch_array($result);
-    $nomInsti=$row[0];
     $asunto = "MobileHunt Android";
     $cuerpo = '
     <html>
     <head>
-    <title>Aviso Importante</title>
+        <title>Aviso Importante</title>
     </head>
     <body>
-    <h1>Aviso Importante</h1>
-    <p>
-    <b>Estimado usuario, le informamos que el dispositivo con los siguientes datos ha sobrepasado la restricción de área asignada '.$phpdate.'.\n
-    </b>
-    Institución/Compañía:'.$nomInsti.'\n
-    Nombre de usuario:'.$usr.'\n
-    Descripción:'.$desc.'\n
-    Latitud:'.$laty.', Longitud: '.$lonx.', '.$bestprov.'\n
-    Para información más detallada favor de dirigirse a la página http://igconsultores.net/raymundo
-    </p>
+        <h1>Aviso Importante</h1>
+        <p>
+            <b>Estimado usuario, le informamos que el dispositivo con los siguientes datos ha sobrepasado la restricción de área asignada<br>
+            </b>
+            <b>Fecha:</b> '.$phpdate.'.<br>
+            <b>Institución/Compañía:</b> '.$nomInsti.'<br>
+            <b>Nombre de usuario:</b> '.$usr.'<br>
+            <b>Descripción:</b> '.$desc.'<br>
+            <b>Latitud:</b> '.$laty.', <b>Longitud:</b> '.$lonx.', '.$bestprov.'<br>
+            Para información más detallada favor de dirigirse a la página http://igconsultores.net/raymundo
+        </p>
     </body>
     </html>
     ';
@@ -54,7 +44,7 @@
     //ruta del mensaje desde origen a destino
     $headers .= "Return-path: raymundoc.vela@hotmail.com\r\n";
 
-    //direcciones que recibián copia
+    //direcciones que recibirán copia
     //$headers .= "Cc: maria@desarrolloweb.com\r\n";
 
     //direcciones que recibirán copia oculta
@@ -62,4 +52,6 @@
 
     if(mail($mail,$asunto,$cuerpo,$headers)) $responsePhp="_1|";
     else $responsePhp="_0|";
+    print(json_encode($responsePhp));
+    mysql_close();
 ?>
